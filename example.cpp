0ee8ba8ff59e87a1be425811ba12bfe526420d09
RanggaGunarto <ranggagunarto@gmail.com>
2024-07-22 14:12:18 +0800

    Add Basic Profiler

---
+++ example.cpp
@@ -0,0 +1,15 @@
+#include "OSVI.h"
+#include <iostream>
+
+int main(){
+    OSVI_PROFILE_BEGIN_SESSION("TEST SESSION", "SESSION.json");
+    OSVI_PROFILE_FUNCTION();
+
+    {
+        OSVI_PROFILE_SCOPE("EXAMPLE SCOPE");
+        std::cout << "Hello World!\n";
+    }
+
+    OSVI_PROFILE_END_SESSION();
+}
+
